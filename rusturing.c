7fef41b3d5d4b58124c065a2fcdf8f2bce27dba3
Without Boats <boats@mozilla.com>
2019-11-06 21:20:14 +0100

    Don't rewrite io_uring_cq_advance
    This improves maintainability by making sure we always track its original
    definition in liburing

--- rusturing.c
+++ rusturing.c
@@ -1,10 +1,6 @@
 #include "liburing.h"
 
-extern inline void rust_io_uring_cq_advance(struct io_uring *ring,
-                                            unsigned nr)
+extern inline void rust_io_uring_cq_advance(struct io_uring *ring, unsigned nr)
 {
-    if (nr) {
-        struct io_uring_cq *cq = &ring->cq;
-        io_uring_smp_store_release(cq->khead, *cq->khead + nr);
-    }
+    io_uring_cq_advance(ring, nr);
 }
