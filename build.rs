056707f2c458d03b4eabef140e4add55a1f5d39a
Without Boats <woboats@gmail.com>
2020-05-12 05:00:52 +0200

    update build script

--- build.rs
+++ build.rs
@@ -1,21 +1,28 @@
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 use cc::Build;
 
 fn main() {
     let project = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).canonicalize().unwrap();
+    let liburing = project.join("liburing");
+
+    // configure script
+    Command::new("./configure").current_dir(&liburing).output().expect("configure script failed");
+
+    let src = liburing.join("src");
 
     // liburing
-    let liburing = project.join("liburing").join("src");
-    Build::new().file(liburing.join("setup.c"))
-                .file(liburing.join("queue.c"))
-                .file(liburing.join("syscall.c"))
-                .file(liburing.join("register.c"))
-                .include(liburing.join("include"))
+    Build::new().file(src.join("setup.c"))
+                .file(src.join("queue.c"))
+                .file(src.join("syscall.c"))
+                .file(src.join("register.c"))
+                .include(src.join("include"))
+                .extra_warnings(false)
                 .compile("uring");
 
     // (our additional, linkable C bindings)
-    Build::new().file(project.join("rusturing.c")).include(liburing.join("include"))
+    Build::new().file(project.join("rusturing.c")).include(src.join("include"))
                 .compile("rusturing");
 }
